539e86fef542c26557b255b40789af3165fda876
Frostiblue <46687289+Frostiblue@users.noreply.github.com>
2019-11-26 17:17:44 -0500

    characterMovement

---
+++ CharacterMovement/Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    public Rigidbody Rb;
+    public GameObject Player;
+    public float horizonatal;
+    public float vertical;
+    public float moveForce;
+    public float jumpForce;
+    public bool isGrounded;
+
+
+    void Update()
+    {
+
+        horizonatal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Player.GetComponent<Rigidbody>().AddForce(Vector3.forward * moveForce);
+            if (Input.GetKeyUp(KeyCode.W))
+            {
+                Player.GetComponent<Rigidbody>().velocity = (Vector3.zero);
+
+            }
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Player.GetComponent<Rigidbody>().AddForce(Vector3.back * moveForce);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Player.GetComponent<Rigidbody>().AddForce(Vector3.right * moveForce);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Player.GetComponent<Rigidbody>().AddForce(Vector3.left * moveForce);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                Player.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce);
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.tag == "Ground" || other.gameObject.tag == "Balloon")
+        {
+            isGrounded = true;
+
+        }
+
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Ground" || other.gameObject.tag == "Balloon")
+        {
+            isGrounded = false;
+        }
+
+    }
+
+
+}
