01d9811654868e316704cc408b2ed10286c98b3c
Zachary Carreiro <43147444+Shadowlet@users.noreply.github.com>
2019-11-27 16:12:54 -0500

    ScoreManager and GameUI almost done.

---
+++ Assets/Scripts/AbilityUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityUI : MonoBehaviour
+{
+    public Image DashIcon;
+    public Image JumpIcon;
+
+    public void SetAbilitiesActive()
+    {
+        DashIcon.GetComponent<SpriteRenderer>().color = new Color(FFFFFF);
+        JumpIcon.GetComponent<SpriteRenderer>().color = new Color(FFFFFF);
+    }
+
+    public void SetAbilitiesInactive()
+    {
+        DashIcon.GetComponent<SpriteRenderer>().color = new Color(313131);
+        JumpIcon.GetComponent<SpriteRenderer>().color = new Color(313131);
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text BalloonText;
+    public Text TimeText;
+    public Text ComboText;
+    public Text TotalScoreText;
+
+    public int balloonCount;
+    public int comboCount;
+
+    public float timeSurvived;
+
+    private int highScore;
+    private int balloonScore;
+    private int comboScore;
+    private int timeScore;
+    private int highestCombo;
+
+    public void CalculateScore()
+    {
+        balloonScore = balloonCount * 3;
+        BalloonText.text = ToString(balloonScore);
+        TimeText.text = ToString(comboScore);
+        ComboText.text = ToString(timeScore);
+        high
+
+    }
+
+    public void SaveHighScore()
+    {
+
+    }
+}
