1d61c3370e8367e2a2b395b884450e3fa708f1cb
Zachary Carreiro <43147444+Shadowlet@users.noreply.github.com>
2019-11-26 19:21:53 -0500

    GameManager almost finished.

---
+++ Assets/Scripts/GameManger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameStates { MENU, GAME }
+
+public delegate void OnStateChange();
+
+public class GameManager
+{
+    protected GameManager() { }
+    private static GameManager gameInstance = null;
+    public event OnStateChangeHandler OnStateChange;
+    public GameStates gameState { get; private set; }
+    public GameManager gameManager { get; set; }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (GameManager.gameInstance == null)
+            {
+                DontDestroyOnLoad(GameManager.gameInstance);
+                GameManager.gameInstance = new GameManager();
+            }
+            return GameManager.gameInstance;
+        }
+
+    }
+
+    public void SetGameState(GameManager state)
+    {
+        this.gameManager = state;
+        OnStateChange();
+    }
+
+
+}
