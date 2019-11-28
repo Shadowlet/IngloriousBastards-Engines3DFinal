b155126578ddaaefcca935a2b1ff43f2eb9f79e6
jmv <44001505+jmvillaescusa@users.noreply.github.com>
2019-11-27 21:43:38 -0500

    Updated Launch timer
    more random than before
    1 - 2.8s instead of a set 1.5s

--- Assets/Scripts/BalloonSpawn.cs
+++ Assets/Scripts/BalloonSpawn.cs
@@ -10,8 +10,8 @@ public class BalloonSpawn : MonoBehaviour
     [SerializeField] private float launchTimer;
     private float balloonLifeTime;
 
-    private float minSpawnTimerRange = 0.12f;
-    private float maxSpawnTimerRange = 0.86f;
+    private float minSpawnTimerRange = 1f;
+    private float maxSpawnTimerRange = 2.8f;
     private float launchTime;
 
     private void Start()
@@ -23,43 +23,40 @@ public class BalloonSpawn : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        // Every time the launchTimer reaches 0, reset the timer
         launchTimer -= 0.01f;
+
         if (launchTimer <= 0)
         {
-            launchTimer = 1.5f;
+            int rNumber = Random.Range(0, 4);
 
-            // Set a random number from 0 to 1, if its 0 then launch balloon, otherwise do nothing
-            int rNumber = Random.Range(0, 5);
-            if (rNumber == 0)
-                launchTimer -= 0.001f;
-
-            if (launchTimer <= 0)
+            if(rNumber == 0)
             {
-                launchTimer = Random.Range(minSpawnTimerRange, maxSpawnTimerRange); // 1.5f initially
-                {
-                    // Spawn balloon and add rigidbody in each spawner
-                    GameObject balloonObject = Instantiate(balloon, balloonSpawner.position, Quaternion.identity, balloonSpawner);
-                    Rigidbody rb = balloonObject.AddComponent<Rigidbody>();
-
-                    int Speed = 7;  //Set speed, for development purposes
-                    // Define an initial speed from 5 to 11
-                    int rSpeed = Random.Range(5, 12);
-
-                    rb.useGravity = false;
-                    rb.velocity = Speed * balloonSpawner.up;
-
-                    // After 7 seconds, remove balloon object
-                    StartCoroutine(RemoveBalloon(balloonObject, balloonLifeTime));
-                }
-
-                IEnumerator RemoveBalloon(GameObject obj, float delay)
-                {
-                    yield return new WaitForSeconds(delay);
-
-                    Destroy(obj);
-                }
+                LaunchBalloon();
             }
+            launchTimer = Random.Range(minSpawnTimerRange, maxSpawnTimerRange);
         }
+    }
+    private void LaunchBalloon()
+    {
+        // Spawn balloon and add rigidbody in each spawner
+        GameObject balloonObject = Instantiate(balloon, balloonSpawner.position, Quaternion.identity, balloonSpawner);
+        Rigidbody rb = balloonObject.AddComponent<Rigidbody>();
+
+        int Speed = 7;  //Set speed, for development purposes
+        // Define an initial speed from 5 to 11
+        int rSpeed = Random.Range(5, 12);
+
+        rb.useGravity = false;
+        rb.velocity = rSpeed * balloonSpawner.up;
+
+        // After 7 seconds, remove balloon object
+        StartCoroutine(RemoveBalloon(balloonObject, balloonLifeTime));
+    }
+                    
+    IEnumerator RemoveBalloon(GameObject obj, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        Destroy(obj);
     }
 }
