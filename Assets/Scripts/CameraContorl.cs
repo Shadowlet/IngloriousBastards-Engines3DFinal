1d3c10d03b2bcfdd0263ef604fb52aa52916a67d
Frostiblue <46687289+Frostiblue@users.noreply.github.com>
2019-11-28 16:40:44 -0500

    Renaming Camera script to CameraContorl

--- Assets/Scripts/CameraContorl.cs
+++ Assets/Scripts/CameraContorl.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class Camera : MonoBehaviour
-{   
+public class CameraContorl : MonoBehaviour
+{
     //Blueprint
 
     public GameObject target;
@@ -12,19 +12,19 @@ public class Camera : MonoBehaviour
     public float distance = 12.0f;
     public float OffsetFromWall = 0.1f;
     public float maxDistance = 20f;
-    public float minDistance  = 0.6f;
-    public float  xSpeed = 200.0f;
-    public float ySpeed =200.0f;
+    public float minDistance = 0.6f;
+    public float xSpeed = 200.0f;
+    public float ySpeed = 200.0f;
     public float yMinLimit = -80f;
-    public float  yMaxLimit = 80f;
+    public float yMaxLimit = 80f;
     public float zoomSpeed = 40f;
     public float autoRotationSpeed = 3.0f;
-    public float autoZoomSpeed = 5.0;
+    public float autoZoomSpeed = 5.0f;
     public LayerMask collisionLayers = 1;
     public bool alwaysRotateToRearOfTarget = false;
-    public bool  allowMouseInputX = true;
+    public bool allowMouseInputX = true;
     public bool allowMouseInputy = true;
-    
+
     private float xDeg = 0.0f;
     private float yDeg = 0.0f;
     private float currentDistance;
@@ -32,31 +32,31 @@ public class Camera : MonoBehaviour
     private float correctedDistance;
     private bool rotateBehind = false;
     private bool mouseSideButton = false;
-    
 
 
-
-
-    // Start is called before the first frame update
     void Start()
     {
-        vector3 angles = transform.eulerAngles;
+        Vector3 angles = transform.eulerAngles;
         xDeg = angles.x;
         yDeg = angles.y;
         currentDistance = distance;
         desiredDistance = distance;
         correctedDistance = distance;
-        if(alwaysRotateToRearOfTarget){
+        if (alwaysRotateToRearOfTarget)
+        {
             rotateBehind = true;
         }
-        
+
     }
 
-    
+
     // camera is moving after everything is  else has been updated
     void LateUpdate()
     {
-        if(allowMouseInputX.GetButtom("Toggle Move"))
-        mouseSideButton = !mouseSideButton;
+        if (allowMouseInputX)
+        {
+            // GetButton("Toggle Move");
+            mouseSideButton = !mouseSideButton;
+        }
     }
 }
