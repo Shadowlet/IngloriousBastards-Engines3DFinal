c1f7cb612bbda41050eb03c9fdbf21c4c9750719
jmv <44001505+jmvillaescusa@users.noreply.github.com>
2019-11-27 20:41:33 -0500

    Added Comments
    Merge Ready

--- Assets/Scripts/BalloonSpawn.cs
+++ Assets/Scripts/BalloonSpawn.cs
@@ -19,13 +19,14 @@ public class BalloonSpawn : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        // Every time the launchTimer reaches 0, reset the timer
         launchTimer -= 0.01f;
-
         if (launchTimer <= 0)
         {
             launchTimer = 1.5f;
+
+            // Set a random number from 0 to 1, if its 0 then launch balloon, otherwise do nothing
             int rNumber = Random.Range(0, 2);
-            Debug.Log(rNumber);
             if(rNumber == 0)
             {
                 LaunchBalloon();
@@ -35,15 +36,16 @@ public class BalloonSpawn : MonoBehaviour
 
     private void LaunchBalloon()
     {
+        // Spawn balloon and add rigidbody in each spawner
         GameObject balloonObject = Instantiate(balloon, balloonSpawner.position, Quaternion.identity, balloonSpawner);
-
         Rigidbody rb = balloonObject.AddComponent<Rigidbody>();
 
+        // Define an initial speed from 5 to 11
         int rSpeed = Random.Range(5, 12);
-
         rb.useGravity = false;
         rb.velocity = rSpeed * balloonSpawner.up;
 
+        // After 7 seconds, remove balloon object
         StartCoroutine(RemoveBalloon(balloonObject, balloonLifeTime));
     }
 
